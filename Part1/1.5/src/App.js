38d585592ccea1d2d1e7a31ae6f0ee98c36e7b70
Sajed-Khan <sajed619007@gmail.com>
2022-06-27 17:55:25 +0400

    Incomplete 1.5
    Problem retrieving elements from array within the object

---
+++ Part1/1.5/src/App.js
@@ -0,0 +1,56 @@
+import './App.css';
+import React from 'react';
+
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course} />
+      <Total parts={course} />
+    </div>
+  )
+}
+
+const Header = (props) =>{
+  return(
+    <div>
+      <h1>{props.course.name}</h1>
+    </div> 
+  )
+}
+
+const Content = (props) =>{
+  return(
+    <div>
+      <p>{props.course.parts[0].name} {props.course.parts[0].exercises}</p>
+      <p>{props.course.parts[1].name} {props.course.parts[1].exercises}</p>
+      <p>{props.course.parts[2].name} {props.course.parts[2].exercises}</p>
+    </div>
+  )
+}
+
+const Total = (props) =>{
+  return(
+    <div>Number of exercises {props.course.parts[0].exercises+props.course.parts[1].exercises+props.course.parts[2].exercises}</div>
+  )
+}
+
+export default App
