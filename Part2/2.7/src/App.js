a070b057418df08c1a1d07dd77f971e30f01cbb7
Sajed-Khan <sajed619007@gmail.com>
2022-07-27 18:51:05 +0400

    2.7 commit

---
+++ Part2/2.7/src/App.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([{id: 1, name: 'Arto Hellas' }]) 
+  const [newName, setNewName] = useState('')
+
+  const addName = (event)=>{
+    event.preventDefault()
+
+    const newGuy ={
+      name: newName,
+      id: persons.length + 1
+    }
+
+    if(persons.find(persons=>persons.name===newName)){
+      //console.log('found the dude')
+      window.alert(newName + " is already added to phonebook")
+    }
+    else{
+      setPersons(persons.concat(newGuy))
+      setNewName('')
+    }
+  } 
+
+  const handleAdd = (event) =>{
+    //console.log(event.target.value)
+    setNewName(event.target.value)
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value = {newName} onChange = {handleAdd}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      <div>
+          {persons.map(person =><p key={person.id}>{person.name}</p>)}
+      </div>
+    </div>
+  )
+}
+
+export default App
